5bbc2187601c632a45f864c818f6ae91434ffa69
Luis Enrique Romero Pérez <wichobros@gmail.com>
2023-01-07 17:52:11 -0600

    Añadida ui y pruebas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UiComponent } from './ui/ui.component';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
+    UiComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
